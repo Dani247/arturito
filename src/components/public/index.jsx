804a6b10de1c7abca22a55a055d3f6052a674310
Carlos Vera <carlos.vera@itexico.com>
2019-03-08 15:06:14 -0600

    Refactor container routes public

--- src/components/public/index.jsx
+++ src/components/public/index.jsx
@@ -3,18 +3,16 @@ import React from 'react'
 import { Router } from "@reach/router";
 import Error404 from '../../shared/components/Error404'
 import Header from '../../shared/components/Header/public'
-// Styles
-import injectSheet from 'react-jss'
-import styles from './stylesHeaderP'
+
 // Lazy components
 const Login = React.lazy(() => import("../../components/public/Login/"));
 const Register = React.lazy(() => import('../../components/public/Register/'))
 const Home = React.lazy(() => import("../../components/public/Home/"));
 
 
-function PublicContainer ({ classes }) {
+const PublicContainer = () => {
   return (
-    <div className={classes.publicContainer}>
+    <>
       <Header />
       <Router className='routerContainer' primary={false}>
         <Home path='home' />
@@ -22,9 +20,8 @@ function PublicContainer ({ classes }) {
         <Register path='register' />
         <Error404 default />
       </Router>
-    </div>
+    </>
   )
 }
 
-const containerPublicWithStyles = injectSheet(styles)(PublicContainer)
-export default containerPublicWithStyles
+export default PublicContainer
