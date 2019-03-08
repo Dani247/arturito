bea71696aed2a1f279a2aeb907de74ef5a3eacc9
Carlos Vera <carlos.vera@itexico.com>
2019-03-08 09:00:09 -0600

    fixe bug with styles

--- src/components/stylesApp.js
+++ src/components/stylesApp.js
@@ -4,11 +4,20 @@ const styles = ({
     width: '100%',
     display: 'grid',
     gridTemplateAreas: `
-    'head    main    main'
-    'head    main    main'
-    'head    main    main' `,
-    gridAutoRows: '3rem auto 3rem',
-    gridAutoColumns: '6rem auto auto'
+    'head'
+    'main'`,
+    gridAutoRows: '2rem auto',
+    gridAutoColumns: '100%'
+  },
+  '@media (min-width: 500px)':{
+    appContainer:{
+      gridTemplateAreas: `
+        'head    main    main'
+        'head    main    main'
+        'head    main    main' `,
+      gridAutoRows: '3rem auto 3rem',
+      gridAutoColumns: '6rem auto auto'
+    }
   }
 })
 export default styles
