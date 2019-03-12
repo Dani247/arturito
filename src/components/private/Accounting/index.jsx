de9f60f457728b991bfce91e6e6b5eaaba7791b4
Daniel Diaz <danie.infante@itexico.com>
2019-03-12 09:46:21 -0600

    charts

--- src/components/private/Accounting/index.jsx
+++ src/components/private/Accounting/index.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
-const Accounting = () => (
-  <div>
-    <h1>Accounting</h1>
+// * styles
+import InjectSheet from 'react-jss'
+import styles from './stylesAccounting'
+
+// * redux
+
+// * charts
+import { Line } from 'react-chartjs-2'
+
+const Accounting = ({ classes }) => (
+  <div className={classes.accountingContainer}>
+    <h3>Saldo total: <small>$26.000 MXN</small></h3>
+    <hr />
+    <Line data={{
+      labels: ['Ene', 'Feb', 'Mar'],
+      datasets: [{
+        label: 'Ingresos',
+        data: [1400, 2000, 3000, 1000, 200],
+        backgroundColor: ['green'],
+        borderColor: ['yellow'],
+        borderWidth: 1
+      }, {
+        label: 'Gastos',
+        data: [800, 600, 300, 600, 39],
+        backgroundColor: ['red'],
+        borderColor: ['yellow'],
+        borderWidth: 1
+      }]
+    }} options={{
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      steppedLine: true
+    }} />
   </div>
 )
 
-export default Accounting
+export default InjectSheet(styles)(Accounting)
--- src/components/private/Accounting/stylesAccounting.js
+++ src/components/private/Accounting/stylesAccounting.js
@@ -1,3 +1,7 @@
 export default theme => ({
-
+  accountingContainer: {
+    width: '100%',
+    height: '100%',
+    padding: '1em'
+  }
 })
