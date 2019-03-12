1b4a1400303977a6dcedd39da909a26e29e23bc3
Daniel Diaz <danie.infante@itexico.com>
2019-03-12 09:52:34 -0600

    charts

--- src/components/private/Accounting/stylesAccounting.js
+++ src/components/private/Accounting/stylesAccounting.js
@@ -2,9 +2,7 @@ export default theme => ({
   accountingContainer: {
     width: '100%',
     minHeight: '100%',
-    display: 'grid',
-    backgroundImage: theme.colorsCombined.gradientBackground.backgroundColor,
-    color: theme.colorsCombined.gradientBackground.color
+    display: 'grid'
   },
   sectionAccounting: {
     display: 'flex',
