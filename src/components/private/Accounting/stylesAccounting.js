bec57221cbcd91fd286ceaf5363c4f5d58be8725
Carlos Vera <carlos.vera@itexico.com>
2019-03-13 15:09:12 -0600

    styles accounting ready

--- src/components/private/Accounting/stylesAccounting.js
+++ src/components/private/Accounting/stylesAccounting.js
@@ -4,6 +4,8 @@ export default theme => ({
     minHeight: '100%',
     display: 'grid',
     gridAutoRows: 'min-content',
+    gridColumns: '1fr 1fr',
+    gridRows: '1fr 1fr'
   },
   sectionAccounting: {
     display: 'flex',
@@ -13,17 +15,34 @@ export default theme => ({
   infoContainer: {
     display: 'flex',
     flexFlow: 'column wrap',
+    alignItems: 'center',
     marginTop: '1em'
   },
   infoSection: {
-    width: '100%',
+    border: '1px solid black',
+    borderRadius: '10px',
+    width: '90%',
     minHeight: '30%',
     display: 'grid',
+    gridTemplateColumns: '2fr 1fr',
+    gridTemplateRows: 'repeat(2, min-content)',
     gridAutoRows: '2em',
     justifyItems: 'start',
-    gridTemplateColumns: 'repeat(2, 1fr)',
+    padding: '1rem',
+    marginBottom: '10px',
     '& > *': {
       margin: 'auto'
     }
+  },
+  infoData: {
+    gridRow: '1/3',
+    gridColumn: '1/2',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  infoButton: {
+    gridRow: '1/3',
+    gridColumn: '2/3'
   }
 })
