7f27dfc821364260e4f4364192721436154660c0
Carlos Vera <carlos.vera@itexico.com>
2019-03-14 09:10:42 -0600

    styles

--- src/components/private/Accounting/Incomes/incomesStyles.js
+++ src/components/private/Accounting/Incomes/incomesStyles.js
@@ -6,7 +6,56 @@ const styles = theme => ({
   buttonAdd: {
     position: 'fixed',
     bottom: '0',
-    right: '10px'
+    right: '10px',
+    transform: 'tranlateX(0)',
+    '&:before':{
+      content: "''",
+      background: 'white',
+      position: 'absolute',
+      width: '70px',
+      height: '70px',
+      borderRadius: '50%'
+    }
+  },
+  iconReturnButtom: {
+    position: 'fixed',
+    height: '0',
+    zIndex: '100'
+  },
+  titleIncomes: {
+    textAlign: 'center',
+    marginBottom: '10px'
+  },
+  dataContainer: {
+    display: 'flex',
+    flexFlow: 'column wrap',
+    alignItems: 'center',
+    padding: '1em'
+  },
+  niceButton: {
+    ...theme.colorsCombined.C,
+    width: '50vw',
+    padding: '2vmin',
+    marginTop: '15px',
+    cursor: 'pointer'
+  },
+  inputContainer: {
+    width: '100%',
+    display: 'flex',
+    flexFlow: 'column wrap',
+    alignItems: 'center',
+    marginBottom: '15px',
+    '& > input': {
+      border: 'none',
+      outline: 'none',
+      borderBottom: '1px solid black',
+      padding: '5px 5px 5px 5px'
+    }
+  },
+  selectContainer: {
+    marginBottom: '10px',
+    border: '1px solid black',
+    cursor: 'pointer'
   }
 })
 
